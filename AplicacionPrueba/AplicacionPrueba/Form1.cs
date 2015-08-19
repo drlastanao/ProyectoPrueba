02322c66648c66cc4b7bf14e42a1969aae8da7a1
drlastanao <drlastanao@gmail.com>
2015-08-19 18:16:23 +0200

    añadido pulsar2
    añadido pulsar2

--- AplicacionPrueba/AplicacionPrueba/Form1.cs
+++ AplicacionPrueba/AplicacionPrueba/Form1.cs
@@ -21,5 +21,10 @@ namespace AplicacionPrueba
         {
             MessageBox.Show("hola mundo!");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Adios!");
+        }
     }
 }
